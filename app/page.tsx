c7318373a5c703e7e275d335d6ff4b94a09176ec
KamishkaJamshaid <kamishkajamshaid8@gmail.com>
2024-08-19 01:49:22 +0500

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "./component/attendifyHero";
 import NavBar from "./component/navbar1";
+import Dashboard from "./dashboard/page";
 
 export default function HomePage(){
  
@@ -7,8 +8,8 @@ export default function HomePage(){
     <div className="min-h-screen bg-slate-100">
       <NavBar prop={{prop:false}}/>
       <HeroSection />
+      <Dashboard prop={{prop:true}}/>
     </div>
-    
     
   )
 }
